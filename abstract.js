ed5c2622f6e0f54d83e13c5603a9a776d05cdcc0
kimdal <kimdal1029@naver.com>
2021-10-28 19:42:14 +0900

    change ellipse -> line

--- abstract.js
+++ abstract.js
@@ -1,27 +1,27 @@
 class abstract{
   constructor(){
-  this.pos = createVector(random(width), random(height));
+  this.pos = createVector(0, 0);
     this.vel = createVector(random(-10, 10), random(-10, 10));
     this.acc = createVector();
-    this.w = random(50, 100);
+    //this.w = random(50, 100);
     this.topspeed = 10;
   }
 
 
   update(){
-  let mouse = createVector(mouseX, mouseY);
-  this.acc = p5.Vector.sub(mouse, this.pos);
-  this.acc.setMag(50);
+  //let mouse = createVector(mouseX, mouseY);
+  //this.acc = p5.Vector.sub(mouse, this.pos);
+  this.acc.setMag(10);
   this.vel.add(this.acc);
   this.vel.limit(this.topspeed);
   this.pos.add(this.vel);
   }
 
   display() {
-  noFill();
-  stroke(120, 50);
-  strokeWeight(1);
-  ellipse(this.pos.x, this.pos.y, this.w, this.w);
+  //noFill();
+  stroke(0, random(0, 255), random(0, 255), 40);
+  strokeWeight(0.5);
+  line(this.pos.x, this.pos.y, this.pos.x*0.2, this.pos.y*0.2);
     }
 
   edges(){
