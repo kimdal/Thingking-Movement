90a208ed6a5a2f77938fa9ba0197b54b30fd2e13
kimdal <kimdal1029@naver.com>
2021-10-28 19:56:52 +0900

    change name

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let Abstract = [];
+let Light = [];
 let Shape = [];
 let Lump = [];
 
@@ -8,7 +8,7 @@ function setup() {
   createCanvas(640, 360);
 
   for (let i=0; i < 50; i++){
-  Abstract[i] = new abstract();
+  Light[i] = new light();
 
     for (let i=0; i < 100; i++){
   Shape[i] = new shape();
@@ -32,10 +32,10 @@ function setup() {
 
 function draw() {
   background(20, 5);
-  for (let i=0; i < Abstract.length; i++){
-  Abstract[i].update();
-  Abstract[i].display();
-  Abstract[i].edges();
+  for (let i=0; i < Light.length; i++){
+  Light[i].update();
+  Light[i].display();
+  Light[i].edges();
   }
   for (let i=0; i < Shape.length; i++){
   Shape[i].update();
